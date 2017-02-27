a72eadd6d44703ba474f3697e1a6acf49deab57b
Sm0ke0ut <sm0ke0ut@fayntic.com>
2017-02-27 15:58:41 +0000

    100% coverage for StartCommand :-)

--- Tests/Command/StartCommandTest.php
+++ Tests/Command/StartCommandTest.php
@@ -3,6 +3,7 @@
 namespace MyOnlineStore\Bundle\RabbitMqManagerBundle\Tests\Command;
 
 use MyOnlineStore\Bundle\RabbitMqManagerBundle\Command\StartCommand;
+use MyOnlineStore\Bundle\RabbitMqManagerBundle\Exception\Supervisor\SupervisorAlreadyRunningException;
 use MyOnlineStore\Bundle\RabbitMqManagerBundle\Generator\RabbitMqConfigGeneratorInterface;
 use MyOnlineStore\Bundle\RabbitMqManagerBundle\Supervisor\SupervisorInterface;
 use Symfony\Component\Console\Tester\CommandTester;
@@ -51,6 +52,33 @@ class StartCommandTest extends BaseCommandTest
 
         $generator->expects($this->once())->method('generate');
         $supervisor->expects($this->once())->method('start');
+
+        $commandTester = new CommandTester($this->command);
+        $commandTester->execute([
+            '--generate' => true,
+        ]);
+    }
+
+    public function testExecuteAlreadyRunning()
+    {
+        $generator = $this->getMock(RabbitMqConfigGeneratorInterface::class);
+        $supervisor = $this->getMock(SupervisorInterface::class);
+
+        $this->container->method('get')->willReturnCallback(function ($service) use ($generator, $supervisor) {
+            switch ($service) {
+                case 'myonlinestore_rabbitmq_manager.config_generator':
+                    return $generator;
+                case 'myonlinestore_rabbitmq_manager.supervisor':
+                    return $supervisor;
+            }
+
+            throw new ServiceNotFoundException($service);
+        });
+
+        $generator->expects($this->once())->method('generate');
+        $supervisor->expects($this->once())->method('start')->willThrowException(
+            new SupervisorAlreadyRunningException()
+        );
 
         $commandTester = new CommandTester($this->command);
         $commandTester->execute([
